cbad48ca9787f7ce74be684fc44306968d40294f
Ebere Abanonu <eaba@users.noreply.github.com>
2022-12-10 18:55:12 +0100

    new PR

---
+++ src/Akka.TestKit.MsTest.Tests/TestKitTestFixtureTest.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestKitTests.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Akka.Actor;
+using Akka.TestKit.TestActors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Akka.TestKit.MsTest.Tests
+{
+    [TestClass]
+    public class TestKitTestFixtureTest : TestKit
+    {
+        [TestMethod]
+        public void Can_create_more_than_one_test_in_a_fixture_with_the_same_actor_name_test1()
+        {
+            Sys.ActorOf<BlackHoleActor>("actor-name");
+        }
+
+        [TestMethod]
+        public void Can_create_more_than_one_test_in_a_fixture_with_the_same_actor_name_test2()
+        {
+            Sys.ActorOf<BlackHoleActor>("actor-name");
+        }
+    }
+}
