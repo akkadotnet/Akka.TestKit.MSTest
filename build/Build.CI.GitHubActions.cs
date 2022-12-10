9922d403b74e88fc6070ddf5be86e8f9bbe867ff
Ebere Abanonu <eaba@users.noreply.github.com>
2022-12-10 23:29:30 +0100

    [GitHubActionsJob] .NET 7

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -52,7 +52,7 @@ class CustomGitHubActionsAttribute : GitHubActionsAttribute
     {
         var job = base.GetJobs(image, relevantTargets);
         var newSteps = new List<GitHubActionsStep>(job.Steps);
-        foreach (var version in new[] { "6.0.*", "5.0.*" })
+        foreach (var version in new[] { "7.0.*" })
         {
             newSteps.Insert(1, new GitHubActionsSetupDotNetStep
             {
