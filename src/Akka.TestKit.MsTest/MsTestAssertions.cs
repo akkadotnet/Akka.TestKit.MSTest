4953e49a3c98ecc8c9d871486e979f7f23820a55
Ebere Abanonu <eaba@users.noreply.github.com>
2022-12-12 22:15:14 +0100

    [fixes] License, copyright headers (#23)
    * [fixes] License, copyright headers
    
    * Removed typesafe copyright

--- src/Akka.TestKit.MsTest/MsTestAssertions.cs
+++ src/Akka.TestKit.MsTest/MsTestAssertions.cs
@@ -1,6 +1,5 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="VsTestAssertions.cs" company="Akka.NET Project">
-//     Copyright (C) 2009-2022 Typesafe Inc. <http://www.typesafe.com>
+//-----------------------------------------------------------------------
+// <copyright file="MsTestAssertions.cs" company="Akka.NET Project">
 //     Copyright (C) 2013-2022 Akka.NET project <https://github.com/akkadotnet/akka.net>
 // </copyright>
 //-----------------------------------------------------------------------
