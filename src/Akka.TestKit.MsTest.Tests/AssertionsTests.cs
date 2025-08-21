17dfa6947e03d0e966cdc46eedadb2e683d92718
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-08-22 01:49:56 +0700

    Bump AkkaVersion to 1.6.0

--- src/Akka.TestKit.MsTest.Tests/AssertionsTests.cs
+++ src/Akka.TestKit.MsTest.Tests/AssertionsTests.cs
@@ -4,6 +4,7 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using Akka.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Akka.TestKit.MsTest.Tests
@@ -20,17 +21,15 @@ namespace Akka.TestKit.MsTest.Tests
         }
 
         [TestMethod]
-        [ExpectedException(typeof(AssertFailedException))]
         public void Fail_should_throw()
         {
-            _assertions!.Fail();
+            Assert.ThrowsExactly<AssertFailedException>(() => _assertions!.Fail());
         }
 
         [TestMethod]
-        [ExpectedException(typeof(AssertFailedException))]
         public void AssertTrue_should_throw_on_false()
         {
-            _assertions!.AssertTrue(false);
+            Assert.ThrowsExactly<AssertFailedException>(() => _assertions!.AssertTrue(false));
         }
 
         [TestMethod]
@@ -40,10 +39,9 @@ namespace Akka.TestKit.MsTest.Tests
         }
 
         [TestMethod]
-        [ExpectedException(typeof(AssertFailedException))]
         public void AssertFalse_should_throw_on_true()
         {
-            _assertions!.AssertFalse(true);
+            Assert.ThrowsExactly<AssertFailedException>(() => _assertions!.AssertFalse(true));
         }
 
         [TestMethod]
@@ -54,10 +52,9 @@ namespace Akka.TestKit.MsTest.Tests
 
 
         [TestMethod]
-        [ExpectedException(typeof(AssertFailedException))]
         public void AssertEqual_should_throw_on_not_equal()
         {
-            _assertions!.AssertEqual(42, 4711);
+            Assert.ThrowsExactly<AssertFailedException>(() => _assertions!.AssertEqual(42, 4711));
         }
 
         [TestMethod]
@@ -68,18 +65,95 @@ namespace Akka.TestKit.MsTest.Tests
 
 
         [TestMethod]
-        [ExpectedException(typeof(AssertFailedException))]
         public void AssertEqualWithComparer_should_throw_on_not_equal()
         {
-            _assertions!.AssertEqual(42, 42,(x,y)=>false);
+            Assert.ThrowsExactly<AssertFailedException>(() => _assertions!.AssertEqual(42, 1142, Comparer));
         }
 
         [TestMethod]
         public void AssertEqualWithComparer_should_succeed_on_equal()
         {
-            _assertions!.AssertEqual(42, 4711,(x,y)=>true);
+            _assertions!.AssertEqual(42, 42, Comparer);
         }
 
+        private bool Comparer(int a, int b) => a == b;
+
+        [TestMethod]
+        [DynamicData(nameof(ExceptionData), DynamicDataSourceType.Method)]
+        public void Generic_AssertThrows_should_succeed_only_on_specific_exception(Exception exception)
+        {
+            if (exception is ConfigurationException ex)
+            {
+                // Should pass and return the same exception instance
+                var returned = _assertions!.AssertThrows<ConfigurationException>(ThisThrows);
+                Assert.AreEqual(ex, returned);
+            }
+            else
+            {
+                // Should fail the assertion since it's not the expected type
+                Assert.ThrowsExactly<AssertFailedException>(() =>
+                    _assertions!.AssertThrows<ConfigurationException>(ThisThrows));
+            }
+
+            void ThisThrows()
+            {
+                throw exception;
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(ExceptionData), DynamicDataSourceType.Method)]
+        public async Task Generic_AssertThrows_should_succeed_only_on_specific_exception_async(Exception exception)
+        {
+            if (exception is ConfigurationException ex)
+            {
+                var returned = await _assertions!.AssertThrowsAsync<ConfigurationException>(ThisThrows);
+                Assert.AreEqual(ex, returned);
+            }
+            else
+            {
+                await Assert.ThrowsExactlyAsync<AssertFailedException>(async () =>
+                    await _assertions!.AssertThrowsAsync<ConfigurationException>(ThisThrows));
+            }
+
+            async Task ThisThrows()
+            {
+                await Task.Yield();
+                throw exception;
+            }
+        }
+
+        [TestMethod]
+        public void Generic_AssertThrows_should_fail_when_no_exception_was_thrown()
+        {
+            Assert.ThrowsExactly<AssertFailedException>(() =>
+                _assertions!.AssertThrows<ConfigurationException>(ThisDoesNotThrow));
+
+            void ThisDoesNotThrow()
+            {
+            }
+        }
+
+        [TestMethod]
+        public async Task Generic_AssertThrows_should_fail_when_no_exception_was_thrown_async()
+        {
+            await Assert.ThrowsExactlyAsync<AssertFailedException>(async () =>
+                await _assertions!.AssertThrowsAsync<ConfigurationException>(ThisDoesNotThrow));
+
+            async Task ThisDoesNotThrow()
+            {
+                await Task.Yield();
+            }
+        }
+
+        // Dynamic data source
+        public static IEnumerable<object[]> ExceptionData()
+        {
+            yield return [new Exception("Wheee")];
+            yield return [new ConfigurationException("Wheee")];
+            yield return [new TimeoutException("Wheee")];
+            yield return [new OperationCanceledException("Wheee")];
+        }
     }
 }
 
